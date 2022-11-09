4f66bde3eb6e0b5729b4f7ed44ba41cd068830e5
EmmanueleD <emmanuele.online@gmail.com>
2022-11-09 16:13:30 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+
+
+  <div class="aggionramento">
+    <h1>Aggiornamento disponibile</h1>
+    <button @click="refreshApp"> Aggiorna... </button>
+  </div>
+
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import update from './mixin/update';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  mixins: [update]
 }
 </script>
 
@@ -22,5 +32,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.aggionramento {
+  background-color: maroon;
+  color: aliceblue;
+  margin: 2rem 0;
+  padding: 2rem 4rem;
 }
 </style>
